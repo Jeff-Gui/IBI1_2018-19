110abac9fb091f9ec471217eb3ddd86fe37a8061
Jeff-Gui <1964728070@qq.com>
2019-03-13 09:27:42 +0800

    Added helloworld.py and variable.py

---
+++ Practical4/helloworld.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Mar 13 09:14:54 2019
+
+@author: jefft
+"""
+
+a = "hello"
+b = "world!"
+print(a+b)
---
+++ Practical4/variable.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Mar 13 09:17:58 2019
+
+@author: jefft
+"""
+#simple math
+a = 820
+b = 592592
+print(b % 7 == 0)
+c = b/7
+d = c/11
+e = d/13
+
+#Boolean variables
+X = True
+Y = False
+Z = (X and not Y) or (Y and not X)
+W = (X != Y)
+print(Z == W)
+
+X = True
+Y = True
+Z = (X and not Y) or (Y and not X)
+W = (X != Y)
+print(Z == W)
+
+X = False
+Y = False
+Z = (X and not Y) or (Y and not X)
+W = (X != Y)
+print(Z == W)
+
+X = False
+Y = True
+Z = (X and not Y) or (Y and not X)
+W = (X != Y)
+print(Z == W)
+
