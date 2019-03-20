3f3cbfdaca92adb1eb9d734367d66702229bf109
Jeff-Gui <1964728070@qq.com>
2019-03-20 10:54:55 +0800

    edited practical_task2, added comments

--- Practical5/practical5_task2.py
+++ Practical5/practical5_task2.py
@@ -6,16 +6,20 @@ Created on Wed Mar 20 09:46:47 2019
 @author: jefft
 """
 
-words = str(input('give me a string of words:'))
+words = input('give me a string of words:')
 mylist = words.split(' ')
+
 for i in range(len(mylist)):
     mylist[i] = mylist[i][::-1]
 """
 why cannot use 
 for string in mylist:
     string = string[::-1]
+answer: string[::-1] create a new list, by giving a reference to the string, it won't change the object also called by string
 """
 mylist.sort()
+#.sort() output NONE,
+#therefore, sorted(list_name) == list_name.sort()
 mylist.reverse()
 
 print(mylist)
