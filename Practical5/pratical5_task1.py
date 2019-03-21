ecb90156032cef57b701663a5ed32d07f5181b45
Jeff-Gui <1964728070@qq.com>
2019-03-21 10:52:12 +0800

    added comments(tutorial) p5

--- Practical5/pratical5_task1.py
+++ Practical5/pratical5_task1.py
@@ -9,7 +9,7 @@ Created on Wed Mar 20 08:59:06 2019
 """
 counting DNA nucleotides
 """
-dna = str(input('input a DNA sequence:'))
+dna = str(input('input a DNA sequence:\n'))
 dic = {}
 dic['A'] = 0
 dic['T'] = 0
@@ -26,13 +26,15 @@ for char in dna:
         dic['G'] += 1
 #PIE PLOT
 import matplotlib.pyplot as plt
-labels = 'A', 'T', 'C', 'G'
-sizes = [dic['A']/len(dna), dic['T']/len(dna), dic['C']/len(dna), dic['G']/len(dna)]
+labels = ['A', 'T', 'C', 'G']
+sizes = [dic['A'], dic['T'], dic['C'], dic['G']]
+    #autopct will translate data in to pct and sizes value is not needed
 #to set the title of the pie chart, use plt.title
 plt.title('pie of the four DNA nucleotides')
-colors = 'mistyrose', 'powderblue', 'mintcream', 'thistle'
-plt.pie(sizes, labels = labels, autopct = '%1.1f%%', colors = colors)
+colors = ['mistyrose', 'powderblue', 'mintcream', 'thistle']
+explode= [0, 0.1, 0, 0]
+plt.pie(sizes, labels = labels, autopct = '%1.1f%%', colors = colors, explode = explode, startangle = 30) 
 
+plt.show()
 print(dna)
 print(dic)
-plt.show()
