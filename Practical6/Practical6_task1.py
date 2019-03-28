86efb0f6ecc0008d96a95a6a5f35e7d5401dcb41
Jeff-Gui <1964728070@qq.com>
2019-03-28 11:08:50 +0800

    polishment
    practical6_task1

--- Practical6/Practical6_task1.py
+++ Practical6/Practical6_task1.py
@@ -11,7 +11,7 @@ import smtplib
 from email.mime.text import MIMEText
 from email.header import Header
 
-#-----FIND LEGAL ADDRESS-------
+#-----FIND LEGAL ADDRESSES-------
 fl1 = open('address_information.csv')
 adlist = []
 nmlist = []
@@ -30,17 +30,21 @@ for line in fl1:
         print(mylist[1],':Wrong Adress!')
 fl1.close()
     
-#-----SEND EMAIL-------
+#-----SEND EMAILS-------
+    
     #collect information
-sender = input('sender address:')
+sender = input('sender address: \n')
 sev_ad = 'smtp.' + re.findall('@(.+)', sender)[0] #deduce server name
-pw = input('password:')
+import getpass
+pw = getpass.getpass('password: ') #collect password while not showing in the console *QtConsole does not support the feature
+    
     #read email body from body.txt
 body = open('body.txt')
 text = ''
 for line in body:
     text += line #read body line by line
 body.close()
+    
     #send email
 server = smtplib.SMTP(sev_ad,25) #setup SMTP object, port for server is 25
 server.login(sender, pw) #connect and login can be done at the same time, or use server.connect() to connect first
@@ -49,15 +53,15 @@ print('From:'+ sender)
 for i in range(0,len(adlist)):
     correct_text = re.sub(r'User', nmlist[i], text) #replace 'User' with correct name
         #setup message, plain means no-format, utf-8 ensures Chinese character can be read
-    msg = MIMEText(correct_text, 'plain', 'utf-8')
+    msg = MIMEText(correct_text, 'plain', 'utf-8') #decoding
     msg['Subject'] = Header(sblist[i], 'utf-8')
     msg['From'] = Header(sender, 'utf-8')
-    msg['To'] =  Header(adlist[i], 'utf-8')
+    msg['To'] =  Header(adlist[i], 'ascii')
         #actually send the email
     try:
         server.sendmail(sender, [adlist[i]], str(msg))
         print ("Mail sent successfully!")
     except smtplib.SMTPException:
         print ("Error")
-    
+
 server.quit()
