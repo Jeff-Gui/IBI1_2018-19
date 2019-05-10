4b92e89ac92543afbda12de6c568c522eec58cc4
Jeff-Gui <1964728070@qq.com>
2019-05-10 14:27:50 +0800

    practical 12 edited, reflection added

--- Practical12/SIR_vaccination.py
+++ Practical12/SIR_vaccination.py
@@ -7,6 +7,7 @@ Created on Wed May  8 10:06:14 2019
 """
 import numpy as np
 import matplotlib.pyplot as plt
+from matplotlib import cm
 
 N = 10000 #total populaiton
 sp = []
@@ -17,12 +18,14 @@ beta = 0.3
 gamma = 0.05
 count1 = 0
 count2 = 0
-for i in range(0,100,10):
+for i in range(0,110,10):
     vc =int(N*i/100)
     if N==vc:
-        sp=[N-vc]
-    sp=[N-vc-1]
-    ift=[1]
+        sp=[0]
+        ift=[0]
+    else:
+        sp=[N-vc-1]
+        ift=[1]
     rcv=[0]
     for t in range(1,1000):
         for item in np.random.choice(range(2),sp[t-1],p=[1-beta*ift[t-1]/N, beta*ift[t-1]/N]): #yield infected people
@@ -39,8 +42,8 @@ for i in range(0,100,10):
 #========================plot==================================================
 plt.figure(figsize=(6,4),dpi=150)
 x = [y for y in range(1,1001)]
-for u in range(0,10):
-    plt.plot(x,list_inf[u],label=str(u*10)+'%')
+for u in range(0,11):
+    plt.plot(x,list_inf[u],label=str(u*10)+'%',color=cm.gnuplot2(u*20))
 plt.xlabel('time')
 plt.ylabel('number of people')
 plt.legend()
--- Practical12/spatial_SIR.py
+++ Practical12/spatial_SIR.py
@@ -6,16 +6,32 @@ Created on Wed May  8 10:42:31 2019
 @author: jefft
 """
 """
--1,1   0,1    1,1
--1,0          1,0
--1,-1  0,-1   1,-1
+Use where() to find infected points
+    - return tuple with 2 lists (x axis and y axis)
+1. for each infected points
+determine if it will recover this time
+    set value to 0.5 with possibility gamma
+2. find infected points again
+3. determine if its neighbour will be infected this time
+    3.1 create length-8 list with 0/1
+    3.2 map each value to point's neighbours
+        relative position of infected points' neighbours stored in dictionary
+        -1,1   0,1    1,1
+        -1,0          1,0
+        -1,-1  0,-1   1,-1
+    3.3 set value of infected points to 1
+        three special situations:
+            a) neighbour already recovered -> remain 0.5
+            b) neighbour out of the population range -> pass (do nothing)
+            c) negative coordinate(other side of the plot) -> pass (do nothing)
+4. plot every time from t= 0 to 100
 """
 import numpy as np
 import matplotlib.pyplot as plt
 
-population = np.zeros((100,100))
-outbreak = np.random.choice(range(100),2)
-population[outbreak[0],outbreak[1]] = 1
+population = np.zeros((100,100)) #construct the whole population (100*100)
+outbreak = np.random.choice(range(100),2) #random choose an outbreak point
+population[outbreak[0],outbreak[1]] = 1 #infected donoted as 1
 
 
 beta = 0.3
@@ -23,19 +39,19 @@ gamma = 0.05
 dict={0:(-1,1),1:(-1,0),2:(-1,-1),3:(0,-1),4:(1,-1),5:(1,0),6:(1,1),7:(0,1)}
 for p in range(0,100):
     ift = np.where(population==1) #find the infected points
-    rcv = np.random.choice(range(2),len(ift[0]),p=[gamma,1-gamma])
+    rcv = np.random.choice(range(2),len(ift[0]),p=[gamma,1-gamma]) #make a list of possible value(0 for recovr, 1 for stay infected)
     for j in range(0,len(ift[0])):
-        if rcv[j]==0: #these guys will be recovered
+        if rcv[j]==0: #find points to be recovered
             population[ift[0][j],ift[1][j]]=0.5
     ift = np.where(population==1)
     for i in range(0,len(ift[0])):
         trn = np.random.choice(range(2),8,p=[1-beta,beta])
-        rtrn = np.where(trn==1)#these guys, if not recovered, will be infected
+        rtrn = np.where(trn==1) #these guys, if not recovered, will be infected
         for item in rtrn[0]:
-            if ift[0][i]+dict[item][0]>=0 and ift[1][i]+dict[item][1]>=0: #minus nunbers can still be indexed to be other side of the matrix
+            if ift[0][i]+dict[item][0]>=0 and ift[1][i]+dict[item][1]>=0: #negative nunbers can still be indexed to be other side of the matrix
                 try:
                     if population[ift[0][i]+dict[item][0], ift[1][i]+dict[item][1]] ==0: # if not recovered
-                        population[ift[0][i]+dict[item][0], ift[1][i]+dict[item][1]] = 1
+                        population[ift[0][i]+dict[item][0], ift[1][i]+dict[item][1]] = 1 #set infected to 1
                 except:
                     pass
     plt.figure(figsize=(6,4), dpi=150)
