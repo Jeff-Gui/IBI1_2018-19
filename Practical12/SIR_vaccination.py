a3ebc6e7f71fa213ab8094e29e919e8c078d6d5f
Jeff-Gui <1964728070@qq.com>
2019-05-10 18:26:00 +0800

    Reflection added

--- Practical12/SIR_vaccination.py
+++ Practical12/SIR_vaccination.py
@@ -22,19 +22,15 @@ for i in range(0,110,10):
     vc =int(N*i/100)
     if N==vc:
         sp=[0]
-        ift=[0]
+        ift=[0] #if all people are vaccinated
     else:
         sp=[N-vc-1]
         ift=[1]
     rcv=[0]
     for t in range(1,1000):
-        for item in np.random.choice(range(2),sp[t-1],p=[1-beta*ift[t-1]/N, beta*ift[t-1]/N]): #yield infected people
-            if item==1:
-              count1 += 1
+        count1 = np.random.choice(range(2),sp[t-1],p=[1-beta*ift[t-1]/N, beta*ift[t-1]/N]).sum() #yield infected people
+        count2 = np.random.choice(range(2),ift[t-1],p=[1-gamma, gamma]).sum() #yield recovered people
         sp.append(sp[t-1]-count1)
-        for item in np.random.choice(range(2),ift[t-1],p=[1-gamma, gamma]): #yield recovered people
-            if item==1:
-                count2 += 1
         rcv.append(rcv[t-1]+count2)
         ift.append(ift[t-1]+count1-count2)
         count1, count2 = 0, 0 #reset
@@ -43,7 +39,7 @@ for i in range(0,110,10):
 plt.figure(figsize=(6,4),dpi=150)
 x = [y for y in range(1,1001)]
 for u in range(0,11):
-    plt.plot(x,list_inf[u],label=str(u*10)+'%',color=cm.gnuplot2(u*20))
+    plt.plot(x,list_inf[u],label=str(u*10)+'%',color=cm.gnuplot2(u*20)) #plot graphs in one figure with loops
 plt.xlabel('time')
 plt.ylabel('number of people')
 plt.legend()
